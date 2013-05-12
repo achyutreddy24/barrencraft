1ffe27b89e7a7dc36aa8ef45c8d52898cba81905
achyutreddy24 <achyutreddy24@gmail.com>
2013-05-11 22:59:22 -0400

    Reference Directory for all Classes

---
+++ common/achyut/reddy/barrencraft/lib/Reference.java
@@ -0,0 +1,8 @@
+package achyut.reddy.barrencraft.lib;
+
+public class Reference {
+
+	public static final String MOD_ID = "BarrenCraft";
+	public static final String MOD_NAME = "Barren Craft";
+	public static final String VERSION = "0.0.1";
+}
