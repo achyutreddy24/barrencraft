561cd996913aecf48822b4e442ae52152e19e673
achyutreddy24 <achyutreddy24@gmail.com>
2013-05-11 22:58:49 -0400

    BarrenCraft.java

---
+++ common/achyut/reddy/barrencraft/BarrenCraft.java
@@ -0,0 +1,29 @@
+package achyut.reddy.barrencraft;
+
+import achyut.reddy.barrencraft.lib.Reference;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.Init;
+import cpw.mods.fml.common.Mod.PreInit;
+import cpw.mods.fml.common.Mod.PostInit;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION)
+public class BarrenCraft {
+
+	@PreInit
+	public void preInit(FMLPreInitializationEvent event) {
+		
+	}
+	
+	@Init
+	public void Init(FMLInitializationEvent event) {
+		
+	}
+	
+	@PostInit
+	public void PostInit(FMLPostInitializationEvent event) {
+		
+	}
+}
